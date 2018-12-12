5e22fee93b3b1efeda632f0bbc765a4681678427
Johannes Brunswicker <johannes.brunswicker@gmail.com>
2018-12-12 16:16:56 +0100

    - removed action tasks filter

--- lib/ansiblelint/utils.py
+++ lib/ansiblelint/utils.py
@@ -513,9 +513,7 @@ def get_action_tasks(yaml, file):
     block_rescue_always = ('block', 'rescue', 'always')
     tasks[:] = [task for task in tasks if all(k not in task for k in block_rescue_always)]
 
-    return [task for task in tasks if
-            set(['include', 'include_tasks',
-                'import_playbook', 'import_tasks']).isdisjoint(task.keys())]
+    return [task for task in tasks]
 
 
 def get_normalized_tasks(yaml, file):
